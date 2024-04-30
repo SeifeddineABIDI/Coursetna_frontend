b174d7c26cb289374c4ba8bd5c2b22181f7c0a19
Ghofrane Rahmouni <92804111+ghofrane0155@users.noreply.github.com>
2024-04-30 14:45:20 +0100

    add Timer to quiz+display questions (1/2) done

--- src/app/modules/evaluation/components/questions/questions.component.ts
+++ src/app/modules/evaluation/components/questions/questions.component.ts
@@ -12,6 +12,10 @@ export class QuestionsComponent {
   questions: Question[] =[];
   id!:number;
 
+  timer: any;
+  durationInSeconds: number = 60; // Duration: 60 seconds
+  timeRemaining: number;
+
   constructor(private qs:QuestionService,private Act: ActivatedRoute){
 
   }
@@ -20,7 +24,29 @@ export class QuestionsComponent {
     this.id=this.Act.snapshot.params['id'];
     this.questions=this.getAll(this.id) as any;
 
+    this.startTimer();
   }
+/***********timer********** */
+// startTimer(): void {
+//   const durationInSeconds = this.durationInMinutes * 60;
+//   this.timer = setTimeout(() => {
+//     // Timer logic: Close the quiz when the duration ends
+//     alert('Quiz time is up!');
+//     // Implement code to close the quiz or navigate away
+//   }, durationInSeconds * 1000); // Convert duration to milliseconds
+// }
+startTimer(): void {
+  this.timeRemaining = this.durationInSeconds;
+  this.timer = setInterval(() => {
+    this.timeRemaining--;
+    if (this.timeRemaining <= 0) {
+      clearInterval(this.timer);
+      alert('Quiz time is up!');
+      // Implement code to close the quiz or navigate away
+    }
+  }, 1000); // Update every second
+}
+/******end timer************* */
   
   getAll(id: number){
     return this.qs.getAllQuestions(id).subscribe(
@@ -44,10 +70,17 @@ onPrevious(): void {
   }
 }
 
-onSubmit(): void {
+saveQuiz(): void {
+  clearTimeout(this.timer);
+  console.log('Quiz saved!');
   // Logique pour soumettre les réponses et calculer le score
   // Vous pouvez ajouter votre logique ici pour calculer le score en fonction des réponses de l'utilisateur
   console.log(this.userAnswers);
+}
+formatTime(seconds: number): string {
+  const mins: string = Math.floor(seconds / 60).toString().padStart(2, '0');
+  const secs: string = (seconds % 60).toString().padStart(2, '0');
+  return `${mins}:${secs}`;
 }
 
 }
