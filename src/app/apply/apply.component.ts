45c07d824fc84624a8a1b6906cb9252c4fccb710
Souid-Houssem <84329777+Souid-Houssem@users.noreply.github.com>
2024-05-07 01:57:05 +0100

    onfile

--- src/app/apply/apply.component.ts
+++ src/app/apply/apply.component.ts
@@ -42,53 +42,12 @@ export class ApplyComponent implements OnInit {
     );
   }
   
-  /*getCommentsByActualiteId(actualiteId: number): void {
-    this.commentaireService.getAllCommentsByActualiteId(actualiteId)
-      .subscribe(commentaires => {
-        this.commentaires = commentaires;
-  
-      });
+ 
+  onFileChanged(event: any): void {
+    const file = event.target.files[0];
+    // Perform operations with the file here
   }
-
-  ajouterCommentaire(actualite: Actualites): void {
-    this.router.navigate(['/AjoutCommentaire', actualite.id]);
-  }
-
-  editActualite(actualite: Actualites): void {
-    this.router.navigate(['/edit-actualite', actualite.id]);
-  }
-
-  likeCommentaire(commentaireId: number): void {
-    this.commentaireService.incrementerLikes(commentaireId)
-      .subscribe(() => {
-        // Mettre à jour les commentaires après l'incrémentation des likes
-        this.getCommentsByActualiteId(this.actualites[0].id);
-      });
-  }
-
-  dislikeCommentaire(commentaireId: number): void {
-    this.commentaireService.incrementerDislikes(commentaireId)
-      .subscribe(() => {
-        // Après l'incrémentation des dislikes, vérifiez si le commentaire doit être signalé
-        this.commentaireService.getCommentaireById(commentaireId)
-          .subscribe(commentaire => {
-            if (commentaire.nombreDislikes > this.seuilDislikes) {
-              // Si le nombre de dislikes dépasse le seuil, signalez le commentaire
-              this.commentaireService.signalerCommentaire(commentaireId)
-                .subscribe(
-                  () => {
-                    console.log('Commentaire signalé automatiquement : ', commentaireId);
-                    // Mettez à jour la liste des commentaires après le signalement
-                    this.getCommentsByActualiteId(this.actualites[0].id);
-                  },
-                  error => {
-                    console.error('Erreur lors du signalement automatique du commentaire : ', error);
-                  }
-                );
-            }
-          });
-      });
-  }*/
+ 
 }
   
 
