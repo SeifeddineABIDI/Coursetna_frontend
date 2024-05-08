89f411c4c78fde4099e19a2a10df26765e820c1e
Souid-Houssem <84329777+Souid-Houssem@users.noreply.github.com>
2024-05-08 02:21:45 +0100

    final commit

--- src/app/services/cv.service.ts
+++ src/app/services/cv.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Cv } from '../modals/Cv';
 
@@ -15,12 +15,33 @@ export class CvService {
     const formData: FormData = new FormData();
     formData.append('file', file);
     formData.append('cv', new Blob([JSON.stringify(cv)], { type: 'application/json' }));
-
+  
     const req = new HttpRequest('POST', `${this.baseUrl}/cv`, formData, {
       reportProgress: true,
       responseType: 'json'
     });
-
+  
+    console.log('Sending:', req);
     return this.http.request(req);
   }
+  
+
+
+
+  handleFileUpload(file: File): Observable<HttpResponse<any>> {
+    const formData: FormData = new FormData();
+    formData.append('file', file);
+
+    return this.http.post<HttpResponse<any>>(`${this.baseUrl}/upload`, formData, {
+      observe: 'response'
+    });
+  }
+
+
+
+
+
+
+
+
 }
